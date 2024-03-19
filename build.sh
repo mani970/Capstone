6b7839f8de4960e90c6fb068123cda50ef89382d
root <root@ip-172-31-22-201.ap-southeast-2.compute.internal>
2024-03-19 09:25:52 +0000

    modified files

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ function build-and-run() {
   docker build -t webpage:v1 .
 
   # Run Docker container
-  docker run -d -p 8080:8080 webpage:v1
+  docker run -d -p 80:80 webpage:v1
 }
 
 # Call the function to build and run the Docker image
